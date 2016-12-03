8875bf5b762d2c85c23a2322f12ab54481adbdb9
Teija Stearns <teijastearns@Teijas-MacBook-Pro.local>
2016-12-03 14:38:24 -0800

    added temperature sensor

--- IoTTouchDevice/main.js
+++ IoTTouchDevice/main.js
@@ -19,8 +19,11 @@
 /*jshint unused:true, undef:true */
 // see http://www.jslint.com/help.html and http://jshint.com/docs
 
-
+var B = 3975;
 var mraa = require("mraa");
+
+//GROVE Kit A0 Connector --> Aio(0)
+var myAnalogPin = new mraa.Aio(0);
 
 //GROVE Kit Shield D6 --> GPIO6
 //GROVE Kit Shield D2 --> GPIO2
@@ -50,6 +53,19 @@ function startSensorWatch(socket) {
             digital_pin_D6.write(touch_sensor_value);
         }
         last_t_sensor_value = touch_sensor_value;
+        
+        //Temperature sensor
+        var a = myAnalogPin.read();
+        console.log("Analog Pin (A0) Output: " + a);
+        //console.log("Checking....");
+        
+        var resistance = (1023 - a) * 10000 / a; //get the resistance of the sensor;
+        //console.log("Resistance: "+resistance);
+        var celsius_temperature = 1 / (Math.log(resistance / 10000) / B + 1 / 298.15) - 273.15;//convert to temperature via datasheet ;
+        //console.log("Celsius Temperature "+celsius_temperature); 
+        var fahrenheit_temperature = (celsius_temperature * (9 / 5)) + 32;
+        console.log("Fahrenheit Temperature: " + fahrenheit_temperature);
+        socket.emit("message", fahrenheit_temperature);
     }, 500);
 }
 
